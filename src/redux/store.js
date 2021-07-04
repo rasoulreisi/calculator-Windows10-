160d6a2ae2d15bb96bc8d921b87e3048c7285b96
EhsanParsania <ehsan.parsa69@gmail.com>
2021-07-04 18:25:36 +0430

    config redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,13 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
 import {reducer} from './reducer'
 
- const store = createStore(reducer)
- export  {store}
+//  const store = createStore(reducer)
+//  export  {store}
+
+
+
+import {createStore, applyMiddleware, compose} from 'redux'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(reducer, composeEnhancers(applyMiddleware()))
